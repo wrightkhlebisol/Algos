80d40f4bf9e04d1a1e11cd02a0ea4313653b256c
wright <calebogundiya@gmail.com>
2020-06-19 23:55:51 +0100

    Selection sort - resolved swap

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -1,19 +1,19 @@
 let arr = [10, 56, 32, 56, 93, 94, 12, 34, 65]
 
-function bubbleSort(arr){
-	for(let i = 0; i < arr.length; i++){
-		for(j = i; j < arr.length; j++){
+function bubbleSort(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		for (j = i; j < arr.length; j++) {
 			// swap
-			if(arr[j] < arr[i]){
+			if (arr[j] < arr[i]) {
 				// assign the lesser value so its not overriden during swap
 				let lesser = arr[j];
 				arr[j] = arr[i];
 				arr[i] = lesser;
-	
+
 			}
 		}
 	}
-	return(arr);
+	return (arr);
 }
 
-bubbleSort(arr);
+console.log(bubbleSort(arr));
--- sorting/selection.js
+++ sorting/selection.js
@@ -1,23 +1,25 @@
 let arr = [10, 56, 32, 56, 93, 94, 12, 34, 65]
 
-function selectionSort(arr){
+function selectionSort(arr) {
 	for (let i = 0; i < arr.length; i++) {
 		// Assume the first element of the array is the smallest
 		let lesserIndex = i;
-		for (let j = i+1; j < arr.length; j++) {
-			if(arr[j] < arr[lesserIndex]){
+		// console.log(lesserIndex);
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[lesserIndex] > arr[j]) {
 				lesserIndex = j;
 			}
-			
-			// console.log(lesserIndex);
 		}
-		// swap
-		let lesser = arr[lesserIndex];
+		if (arr[i] !== arr[lesserIndex]) {
+			// Swap
+			let lesser = arr[lesserIndex];
 
-		arr[i+1] = arr[i];
-		arr[i] = lesser;
+			arr[lesserIndex] = arr[i];
+			arr[i] = lesser;
+		}
+
 	}
-	console.log(arr)
+	console.log(arr);
 }
 
 selectionSort(arr);
