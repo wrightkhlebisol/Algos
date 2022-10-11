3e967451fb3bd118b70f1785abf17f77e891b6e7
Wright <calebogundiya@gmail.com>
2022-10-11 23:11:05 +0100

    Set Demo

---
+++ src/com/wright/collections/SetDemo.java
@@ -0,0 +1,26 @@
+package com.wright.collections;
+
+import java.util.*;
+
+public class SetDemo {
+    public static void show(){
+        Collection<String> collection = new ArrayList<>();
+        Collections.addAll(collection, "a", "b", "c", "c");
+
+        Set<String> set1 =
+                new HashSet<>(Arrays.asList("a", "b", "c"));
+
+        Set<String> set2 =
+                new HashSet<>(Arrays.asList("b", "c", "d"));
+
+//        Union
+        set1.addAll(set2);
+        System.out.println(set1);
+//        Intersection
+        set1.retainAll(set2);
+        System.out.println(set1);
+//        Difference
+        set1.removeAll(set2);
+        System.out.println(set1);
+    }
+}
