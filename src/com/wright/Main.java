ba54014bc7b9f747c9b0686fa87a9e6f148ed0c5
Wright <calebogundiya@gmail.com>
2022-10-16 12:26:36 +0100

    Implement addFirst method

--- src/com/wright/Main.java
+++ src/com/wright/Main.java
@@ -6,9 +6,10 @@ import com.wright.algoandds.LinkedList;
 public class Main {
     public static void main(String[] args) {
         var list = new LinkedList();
-        list.addLast(10);
-        list.addLast(20);
-        list.addLast(30);
+        list.addFirst(10);
+//        list.addLast(20);
+//        list.addLast(30);
 
+        list.addFirst(5);
     }
 }
