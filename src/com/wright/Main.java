8048f47a7b6af05eca24448034d9f96662bbb4d5
Wright <calebogundiya@gmail.com>
2022-10-16 18:01:44 +0100

    Implement indexOf

--- src/com/wright/Main.java
+++ src/com/wright/Main.java
@@ -6,10 +6,11 @@ import com.wright.algoandds.LinkedList;
 public class Main {
     public static void main(String[] args) {
         var list = new LinkedList();
-        list.addFirst(10);
-//        list.addLast(20);
-//        list.addLast(30);
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
 
         list.addFirst(5);
+        System.out.println(list.indexOf(30));
     }
 }
---
+++ src/com/wright/algoandds/LinkedList.java
@@ -0,0 +1,54 @@
+package com.wright.algoandds;
+
+public class LinkedList {
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+
+    public void addLast (int item) {
+        var node = new Node(item);
+        if(last == null)
+            first = last = node;
+        else
+            last.next = node;
+            last = node;
+    }
+
+    public void addFirst(int item){
+        var node = new Node(item);
+
+        if(first == null)
+            first = last = node;
+        else
+        //  Set the next field of new node to
+        //  current first
+            node.next = first;
+        //  Since first still points to first old
+        //  node, we need to reassign it to new node
+            first = node;
+    }
+
+    public int indexOf(int item){
+        var currentNode = first;
+        int index = 0;
+        while(currentNode != null){
+            if(currentNode.value == item) return ++index;
+            index++;
+            currentNode = currentNode.next;
+        }
+        return -1;
+    }
+//    deleteFirst();
+//    deleteLast();
+//    contains();
+//    IndexOf();
+}
