bd3b5ef1173cf0af01b52402663fdf0cf982efd4
Wright <calebogundiya@gmail.com>
2022-12-10 03:21:41 +0000

    Implement removeLast method

--- src/com/wright/Main.java
+++ src/com/wright/Main.java
@@ -6,12 +6,16 @@ import com.wright.algoandds.LinkedList;
 public class Main {
     public static void main(String[] args) {
         var list = new LinkedList();
-        list.addLast(10);
-        list.addLast(20);
-        list.addLast(30);
+//        list.addLast(10);
+//        list.addLast(20);
+//        list.addLast(30);
+////
+        list.addFirst(5);
 
         list.addFirst(5);
-        list.removeFirst();
-        System.out.println(list.indexOf(10));
+        list.removeLast();
+//        list.removeLast();
+//        list.removeLast();
+        System.out.println(list.size());
     }
 }
--- src/com/wright/algoandds/LinkedList.java
+++ src/com/wright/algoandds/LinkedList.java
@@ -1,67 +1,105 @@
 package com.wright.algoandds;
 
+import java.util.NoSuchElementException;
+
 public class LinkedList {
 
     private class Node {
         private int value;
         private Node next;
 
-        public Node(int value){
+        public Node(int value) {
             this.value = value;
         }
     }
 
     private Node first;
     private Node last;
+    private int size;
 
-    public void addLast (int item) {
+    public void addLast(int item) {
+        ++size;
         var node = new Node(item);
-        if(last == null)
+        if (isEmpty())
             first = last = node;
         else
             last.next = node;
-            last = node;
+        last = node;
     }
 
-    public void addFirst(int item){
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+    public void addFirst(int item) {
+        ++size;
         var node = new Node(item);
 
-        if(first == null)
+        if (isEmpty())
             first = last = node;
         else
-        //  Set the next field of new node to
-        //  current first
+            //  Set the next field of new node to
+            //  current first
             node.next = first;
         //  Since first still points to first old
         //  node, we need to reassign it to new node
-            first = node;
+        first = node;
     }
 
-    public int indexOf(int item){
+    public int indexOf(int item) {
         var currentNode = first;
         int index = 0;
-        while(currentNode != null){
-            if(currentNode.value == item) return ++index;
+        while (currentNode != null) {
+            if (currentNode.value == item) return index;
             index++;
             currentNode = currentNode.next;
         }
         return -1;
     }
 
-    public boolean contains(int item){
+    public boolean contains(int item) {
         return indexOf(item) != -1;
     }
 
-    public void removeFirst(){
+    public void removeFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        --size;
         // [10 -> 20 -> 30]
         // Get second, assign that to global first
         // Assign first next to null
-        Node secondNode = first.next;
+        var secondNode = first.next;
         first.next = null;
         first = secondNode;
     }
 
-    public void removeLast(){
+    public void removeLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        --size;
+//      Handle when list has only one node
+        if (first == last) {
+            //  if it is, set to null
+            first = last = null;
+            return;
+            // else
+        }
+//      Get previous
+        var nodePointer = first;
+        while (nodePointer != null) {
+            if (nodePointer.next.next == null) {
+                last = nodePointer;
+                last.next = null;
+                return;
+            } else {
+                nodePointer = nodePointer.next;
+            }
+        }
+    }
 
+    public int size(){
+        return size;
     }
 }
