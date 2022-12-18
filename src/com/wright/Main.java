a4cc17671b222c8cc52b303fb761eacaadfc65d2
Wright <calebogundiya@gmail.com>
2022-12-18 18:21:22 +0000

    reverseLinkedList

--- src/com/wright/Main.java
+++ src/com/wright/Main.java
@@ -11,14 +11,17 @@ public class Main {
         list.addLast(10);
         list.addLast(20);
         list.addLast(30);
-////
-        list.addFirst(5);
 
-        list.addFirst(5);
+//        list.addFirst(5);
+//        list.addFirst(5);
 //        list.removeLast();
 //        list.removeLast();
 //        list.removeLast();
-        var nodeValueArray = list.toArray();
-        System.out.println(Arrays.toString(nodeValueArray));
+//        var nodeValueArray = list.toArray();
+//        System.out.println(Arrays.toString(nodeValueArray));
+        list.reverseLinkedList();
+        var reversedNodeArray = list.toArray();
+        System.out.println(Arrays.toString(reversedNodeArray));
+
     }
 }
--- src/com/wright/algoandds/LinkedList.java
+++ src/com/wright/algoandds/LinkedList.java
@@ -113,4 +113,40 @@ public class LinkedList {
         }
         return nodeArray;
     }
+
+    public void reverseLinkedList(){
+        // [10 -> 20 -> 30]
+        //
+        if(isEmpty()) return;
+
+        var node1 = first;
+        var node2 = first.next;
+
+        while(node2 != null){
+            // break link to third, store third
+            var node3 = node2.next;
+            node2.next = node1;
+
+            node1 = node2;
+            node2 = node3;
+        }
+        last = first;
+        last.next = null;
+        first = node1;
+    }
+
+
+    /**
+     * //            if(currentNode == last){
+     * //                return;
+     * ////                first.next = null;
+     * //            }
+     * //            System.out.println(currentNode.value);
+     *             // Get Next node,
+     *             var nextNode = currentNode.next;
+     *             System.out.println(nextNode.value);
+     * //            nextNode.next = ;
+     *             currentNode = currentNode.next;
+     *             currentNode.next =
+     */
 }
